b988e8cf8460b9e13cf510b3d2f8e9e073bde5eb
SonTonyD <sontony.dinh@gmail.com>
2022-11-19 15:36:58 +0100

    line identification OK

--- Task2.py
+++ Task2.py
@@ -172,7 +172,7 @@ def replacePixel(image_matrix, lookup_table):
 
 def convolutionOperation(image_matrix, kernel):
     width, height = image_matrix.shape[0], image_matrix.shape[1]
-    copy_image_matrix = image_matrix
+    copy_image_matrix = np.copy(image_matrix)
 
     for i in range(1, width-1):
         for j in range(1, height-1):
@@ -180,10 +180,12 @@ def convolutionOperation(image_matrix, kernel):
             sum_kernel, weighted_sum = 0, 0
             for k in range(-1,2):
                 for l in range(-1,2):
-                    sum_kernel += kernel[k+1,l+1]
+                    sum_kernel += abs(kernel[k+1,l+1])
                     weighted_sum += image_matrix[i+k,j+l]*kernel[k+1,l+1]
             copy_image_matrix[i,j] = weighted_sum/sum_kernel
     return copy_image_matrix
+
+
 
 
 @click.command()
@@ -198,7 +200,7 @@ def convolutionOperation(image_matrix, kernel):
 @click.option('--cflaco', is_flag=True, help='Compute the flattening coefficient (Kurtosis)')
 @click.option('--centropy', is_flag=True, help='Compute the entropy of the image')
 @click.option('--hhyper', nargs=2, type=int, help='Histogram modification with hyperbolic final probability density function; 2 parameters: minBrightness maxBrightness')
-@click.option('--slineid', is_flag=True, help='perform a line identification operation')
+@click.option('--slineid', default=-1, help='perform a line identification operation: 0 -> horizontal, 1 -> vertical, 2 -> diagUp, 3 -> diagDown')
 def operation(name, cmean, cvariance, histogram, cstdev, cvarcoi, casyco, cflaco, cvarcoii, centropy, hhyper, slineid) :
     img = Image.open(name)
     image_matrix = np.array(img)
@@ -214,20 +216,25 @@ def operation(name, cmean, cvariance, histogram, cstdev, cvarcoi, casyco, cflaco
         if function_flags[i]: 
             invokeFunction(function_names[i], functions[i], image_matrix)
 
-    if hhyper != None:
+    if len(hhyper) != 0:
         lookup_table = hhyper_function(image_matrix, hhyper)
         new_matrix = replacePixel(image_matrix, lookup_table)
         Image.fromarray(new_matrix).save("./images/"+name+"hhyper"+".bmp")
         Image.fromarray(new_matrix).show("New Image")
         invokeHistogram(new_matrix, name)
 
+    if slineid != -1:
+        horizontal_kernel = np.array([[-1,-1,-1],[2,2,2],[-1,-1,-1]])
+        vertical_kernel = np.array([[-1,2,-1],[-1,2,-1],[-1,2,-1]])
+        diagonalUp_kernel = np.array([[-1,-1,2],[-1,2,-1],[2,-1,-1]])
+        diagonalDown_kernel = np.array([[2,-1,-1],[-1,2,-1],[-1,-1,2]])
 
-'''
-    if slineid:
-        convolutionOperation(image_matrix)
-        Image.fromarray(new_image_matrix).save("./images/"+name+"slineid"+".bmp")
+        line_detection_kernels = [horizontal_kernel, vertical_kernel, diagonalUp_kernel, diagonalDown_kernel]
+
+        new_image_matrix = convolutionOperation(image_matrix, line_detection_kernels[slineid])
+        Image.fromarray(new_image_matrix).save("./images/"+name+"slineid"+str(slineid)+".bmp")
         Image.fromarray(new_image_matrix).show("New Image")
-'''
+
 
 
     
